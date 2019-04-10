5f8f98310bc92b1bb51b35605f62eb3cc6d69827
Yerzhan Serikbay <yerzhan.tulkubayev@gmail.com>
2019-04-10 14:52:47 +0800

    Initial project setup

---
+++ SimplestView/SimplestView.h
@@ -0,0 +1,19 @@
+//
+//  SimplestView.h
+//  SimplestView
+//
+//  Created by YS on 4/10/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplestView.
+FOUNDATION_EXPORT double SimplestViewVersionNumber;
+
+//! Project version string for SimplestView.
+FOUNDATION_EXPORT const unsigned char SimplestViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplestView/PublicHeader.h>
+
+
---
+++ SimplestViewExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplestViewExamples
+//
+//  Created by YS on 4/10/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
